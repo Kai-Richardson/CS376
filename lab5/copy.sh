f05d6c646469a54bf4e2880ce0a8d741f5df4c29
Kai Richardson <me@kairichardson.us>
2019-10-03 10:34:46 -0700

    lab5 done

---
+++ lab5/copy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp $1 $2
+
+echo Details for $2 for $USER
+
+ls -lh $2
---
+++ lab5/dumb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+myVariable=Hello
+
+anothervar=Fred
+
+echo $myVariable $anothervar
+echo
+
+sampledir=/etc
+ls --color=auto $sampledir
